8e11ac2640408684b57b33a668733fdf0ddf1e28
Arronax <alexandre.petit.qin@gmail.com>
2021-09-26 11:07:02 +0200

    step 1 - refacto and returning another value

--- string-calculator/src/main/java/coding/kata/StringCalculator.java
+++ string-calculator/src/main/java/coding/kata/StringCalculator.java
@@ -7,9 +7,9 @@ public class StringCalculator {
     }
 
     public int calculate(String numbers) {
-        if(numbers.isEmpty()) {
+        if (numbers.isEmpty()) {
             return 0;
         }
-        return 1;
+        return Integer.parseInt(numbers);
     }
 }
--- string-calculator/src/test/java/coding/kata/StringCalculatorShould.java
+++ string-calculator/src/test/java/coding/kata/StringCalculatorShould.java
@@ -1,25 +1,33 @@
 package coding.kata;
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import sun.awt.geom.AreaOp;
 
 @RunWith(JUnit4.class)
 public class StringCalculatorShould {
 
+    private StringCalculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new StringCalculator();
+    }
+
     @Test
     public void return_0_when_empty_string_is_provided() {
-        StringCalculator calculator = new StringCalculator();
         Assert.assertEquals(calculator.calculate(""), 0);
     }
 
     @Test
     public void return_1_when_1_is_provided_as_string() {
-        StringCalculator calculator = new StringCalculator();
         Assert.assertEquals(calculator.calculate("1"), 1);
+    }
+
+    @Test
+    public void return_2_when_2_is_provided_as_string() {
+        Assert.assertEquals(calculator.calculate("2"), 2);
     }
 }
