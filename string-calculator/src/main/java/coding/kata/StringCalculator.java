514b7f091b4010f2720a39e8747876632b39bf29
Arronax <alexandre.petit.qin@gmail.com>
2021-09-26 22:04:15 +0200

    step 5 - handling negative values exception

--- string-calculator/src/main/java/coding/kata/StringCalculator.java
+++ string-calculator/src/main/java/coding/kata/StringCalculator.java
@@ -4,16 +4,16 @@ package coding.kata;
 import java.util.Arrays;
 
 public class StringCalculator {
-
+    private final static String NEGATIVE_NOT_ALLOWED_EXCEPTION = "negatives not allowed";
     private final static String DEFAULT_SEPARATORS = ",|\n";
-
+    private final static int NEW_SEPARATOR_DELIMITER_LENGTH = 4;
     private final static String NEW_SEPARATOR_REGEX = "^/{2}.*\n.*";
+
 
     public StringCalculator() {
     }
 
     public int calculate(String stringValues) {
-
         String[] stringNumberArray;
 
         stringNumberArray = computeStringNumberArray(stringValues);
@@ -37,11 +37,18 @@ public class StringCalculator {
         } else {
             stringNumberArray = stringValues.split(DEFAULT_SEPARATORS);
         }
+        Arrays.stream(stringNumberArray).forEach(stringNumber -> checkNumber(stringNumber));
         return stringNumberArray;
     }
 
+    private void checkNumber(String stringNumber) {
+        if(stringNumber.contains("-")) {
+            throw new IllegalArgumentException(NEGATIVE_NOT_ALLOWED_EXCEPTION);
+        }
+    }
+
     private String extractStringNumberFrom(String stringNumbers, String newSeparator) {
-        return stringNumbers.substring(newSeparator.length() + 4 - 1);
+        return stringNumbers.substring(newSeparator.length() + NEW_SEPARATOR_DELIMITER_LENGTH - 1);
     }
 
     private String extractNewSeparatorFrom(String stringNumbers) {
--- string-calculator/src/test/java/coding/kata/StringCalculatorShould.java
+++ string-calculator/src/test/java/coding/kata/StringCalculatorShould.java
@@ -1,10 +1,12 @@
 package coding.kata;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 @RunWith(JUnit4.class)
 public class StringCalculatorShould {
@@ -18,51 +20,61 @@ public class StringCalculatorShould {
 
     @Test
     public void return_0_when_empty_string_is_provided() {
-        Assert.assertEquals(calculator.calculate(""), 0);
+        assertEquals(calculator.calculate(""), 0);
     }
 
     @Test
     public void return_1_when_1_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("1"), 1);
+        assertEquals(calculator.calculate("1"), 1);
     }
 
     @Test
     public void return_2_when_2_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("2"), 2);
+        assertEquals(calculator.calculate("2"), 2);
     }
 
     @Test
     public void return_2_when_1_comma_1_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("1,1"), 2);
+        assertEquals(calculator.calculate("1,1"), 2);
     }
 
     @Test
     public void return_3_when_1_comma_2_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("1,2"), 3);
+        assertEquals(calculator.calculate("1,2"), 3);
     }
 
     @Test
     public void return_3_when_1_comma_1_comma_1_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("1,1,1"), 3);
+        assertEquals(calculator.calculate("1,1,1"), 3);
     }
 
     @Test
     public void return_3_when_1_new_line_2_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("1\n2"), 3);
+        assertEquals(calculator.calculate("1\n2"), 3);
     }
 
     @Test
     public void return_3_when_1_new_line_1_comma_1_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("1\n1,1"), 3);
+        assertEquals(calculator.calculate("1\n1,1"), 3);
     }
 
     @Test
     public void return_3_using_the_new_delimiter_semicolon_when_1_semicolon_2_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("//;\n1;2"), 3);
+        assertEquals(calculator.calculate("//;\n1;2"), 3);
     }
 
     @Test
     public void return_3_using_the_new_delimiter_TTT_when_1_TTT_2_is_provided_as_string() {
-        Assert.assertEquals(calculator.calculate("//TTT\n1TTT2"), 3);
+        assertEquals(calculator.calculate("//TTT\n1TTT2"), 3);
+    }
+
+    @Test
+    public void throw_an_exception_when_a_negative_value_is_added() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    calculator.calculate("1,-2");
+                });
+        assertEquals("negatives not allowed", exception.getMessage());
     }
 }
